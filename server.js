e842ba127d9619bd4fce77f1058022e08acce427
Hyoeun Lee <scarlettcello@gmail.com>
2018-09-07 20:55:13 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+/* maintenance */
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
+    });
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+// app.get('/', (req, res) => {
+//     // res.send('<h1>Hello Express!</h1>');
+//     res.send({
+//         name: 'Caroline',
+//         likes: [
+//             'Hiking',
+//             'Chocolate'
+//         ]
+//     })
+// });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome to my website!',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        type: 'Bad Request',
+        message: 'Unable to handle request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is on port 3000');
+});
